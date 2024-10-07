b8d273a3a167fc2dc404d3afe6775f6a30723191
SinanErbezci <sinanerbezci@hotmail.com>
2024-10-07 15:22:08 +0300

    new stuff

--- bookclub/library/urls.py
+++ bookclub/library/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>", views.DetailView.as_view(), name="book"),
     path("create_book" , views.create_book, name="create_book")
 ]
--- bookclub/library/views.py
+++ bookclub/library/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse
+from .models import Book,Author
+from .forms import NameForm, ContactForm
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = "library/index.html"
+    context_object_name = "latest_books_added"
+
+    def get_queryset(self):
+        """Return last added 5 books """
+        return Book.objects.all()[:5]
+
+class DetailView(generic.DetailView):
+    model = Book
+    template_name = "library/book.html"
+
 
 def index(request):
     hello = "hello, world"
-    output = {"hello" : hello}
+    books = Book.objects.all()
+    output = {"hello" : hello, "books":books}
     return render(request, "library/index.html",output)
 
 def create_book(request):
-    return HttpResponse("create book page") 
+    if request.method == "POST":
+        form =  ContactForm(request.POST)
+        if form.is_valid():
+            print("sender",form.cleaned_data["sender"])
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        form = ContactForm()
+
+    return render(request, "library/create_book.html", {"form" : form})
